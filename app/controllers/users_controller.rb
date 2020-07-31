7e7ea9afcdc0df87eb375fa961cea4b4bc35a163
Samuel Lopez <lopezsauri.s@gmail.com>
2020-07-31 12:46:29 -0500

    User, Articles restrictions

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,9 @@
 class UsersController < ApplicationController
   before_action :set_user, only: [:show, :edit, :update]
+  before_action :require_user, only: [:edit, :update]
+  before_action :require_same_user, only: [:edit, :update]
 
-  def show
+    def show
     @articles = @user.articles.paginate(page: params[:page], per_page: 5)
   end
 
@@ -43,5 +45,12 @@ class UsersController < ApplicationController
 
  def set_user
      @user = User.find(params[:id])
+ end
+
+ def require_same_user
+   if current_user != @user
+     flash[:alert] = "You can only edit you own account"
+     redirect_to @user
+   end
  end
 end
